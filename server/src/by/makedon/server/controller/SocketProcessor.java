c1934482345c38e5e70d220f77e70f202b156b9d
egor <egor.krutoychel.komp@mail.ru>
2017-11-06 00:30:16 +0300

    added DB to Server

--- server/src/by/makedon/server/controller/SocketProcessor.java
+++ server/src/by/makedon/server/controller/SocketProcessor.java
@@ -4,8 +4,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -36,27 +35,62 @@ public class SocketProcessor implements Runnable {
 
     @Override
     public void run() {
+        logger.log(Level.INFO, "Client " + socket.getInetAddress() + " Thread have run");
+
         final int DELAY = 1_000;
         Timer timer = new Timer();
         timer.schedule(new CloseSocketTimer(), DELAY);
-        InputStream is = null;
 
+        ObjectInputStream objis = null;
+        ObjectOutputStream objos = null;
         try {
-            is = socket.getInputStream();
-            while (isSocketRun) {
+            InputStream is = socket.getInputStream();
+            objis = new ObjectInputStream(is);
+            OutputStream os = socket.getOutputStream();
+            objos = new ObjectOutputStream(os);
 
+            final String PERSON_INFORMATION_KEY = "PERSONINFORMATION";
+            final String SESSION_KEY = "SESSION";
+
+            while (isSocketRun) {
+                try {
+                    final String KEY = (String) objis.readObject();
+                    switch (KEY) {
+                        case PERSON_INFORMATION_KEY:
+                            final String QUERY = (String) objis.readObject();
+                            Controller controller = new Controller();
+                            objos.flush();
+                            objos.writeObject(controller.findPersonInformation(QUERY));
+                            objos.flush();
+                            break;
+                        case SESSION_KEY:
+                            /////////
+                            break;
+                    }
+                } catch (ClassNotFoundException e) {
+                    logger.log(Level.WARN, e);
+                }
             }
         } catch (IOException e) {
             logger.log(Level.WARN, e);
         } finally {
+            logger.log(Level.INFO, "Client " + socket.getInetAddress() + " Thread have stopped");
             timer.cancel();
 
-            if (is != null) try {
-                is.close();
-            } catch (IOException e) {
-                logger.log(Level.WARN, e);
+            if (objis != null) {
+                try {
+                    objis.close();
+                } catch (IOException e) {
+                    logger.log(Level.WARN, e);
+                }
             }
-
+            if (objos != null) {
+                try {
+                    objos.close();
+                } catch (IOException e) {
+                    logger.log(Level.WARN, e);
+                }
+            }
             if (socket != null && !socket.isClosed()) {
                 try {
                     socket.close();
--- server/src/by/makedon/server/database/BookOfReferenceDB.java
+++ server/src/by/makedon/server/database/BookOfReferenceDB.java
@@ -1,5 +1,68 @@
 package by.makedon.server.database;
 
-public class BookOfReferenceDB {
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookOfReferenceDB {
+    private final String USER = "root";
+    private final String URL = "jdbc:mysql://localhost:3306/book_of_reference";
+    private final String PASSWORD = "elzzz147";
+    static Logger logger = LogManager.getLogger(BookOfReferenceDB.class);
+
+    public BookOfReferenceDB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            logger.log(Level.WARN, e);
+        }
+    }
+
+    public List<String> findPersonInformation(final String QUERY) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        List<String> personInformationList = new ArrayList<String>();
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(QUERY);
+
+            while(resultSet.next()) {
+                final String RESULT = resultSet.getString(1) + " " + resultSet.getString(2) + " " +
+                                      resultSet.getString(3) + " " + resultSet.getString(4) + " " +
+                                      resultSet.getString(5) + " " + resultSet.getString(6);
+                personInformationList.add(RESULT);
+            }
+        } catch (SQLException e) {
+            logger.log(Level.WARN, e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    logger.log(Level.WARN, e);
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    logger.log(Level.WARN, e);
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    logger.log(Level.WARN, e);
+                }
+            }
+        }
+        return personInformationList;
+    }
 }
